203a1957bc48a69847febf5ee0b741d114d7bd2a
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 13:44:59 -0700

    add test to not raise error when Player is inititialized with an invalid input hash

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,12 @@ module TicTacToe
   context "#initialize" do 
    it "raises an exception when initialized with {}" do
     expect {Player.new({})}.to raise_error 
-   end 
+   end
+    
+   it "does not raise error when initialized with a valid input hash" do 
+    input = { mark: "X", name: "Someone" }
+    expect { Player.new(input) }.to_not raise_error
+   end  
   end 
  end 
 

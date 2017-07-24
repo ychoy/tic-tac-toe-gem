cda7c785a10f2d938f41adfeea6c52bd6dcf8deb
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 13:59:01 -0700

    instantiate Board class with default or input. Set private method

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,7 +1,14 @@
 module TicTacToe
  class Board
-  def initialize(input)
-   @grid = input.fetch(:grid)
+  def initialize(input={})
+   #Board class can be instantiated with input hash or default value
+   @grid = input.fetch(:grid, default_grid)
+  end 
+
+  private
+  #only called by isntances of the Board class
+  def default_grid
+   Array.new(3) { Array.new(3) {Cell.new} }
   end 
  end 
 end 

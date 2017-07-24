821293c208c4fea1feacf2648071e9384cfc1fc6
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 14:01:29 -0700

    Test that the Board grid has three rows by default and nested rows

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,23 @@ require "spec_helper"
 module TicTacToe
  describe Board do 
   context "#intitialize" do 
+
    it "initializes the board with a grid" do 
     expect { Board.new(grid: "grid") }.to_not raise_error
    end 
+
+   it "sets the grid with three rows by default" do
+    board = Board.new
+    expect(board.grid).to have(3).things
+   end
+ 
+   it "creates three things in each row by default" do
+    board = Board.new
+    board.grid.each do |row|
+     expect(row).to have(3).things
+    end
+   end
+
   end 
  end 
 end 

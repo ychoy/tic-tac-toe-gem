6ff10629dc7cd97d581882215ed95a7d50728f84
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 13:30:40 -0700

    testing that a Cell object is instantiated with no arguments will have default value equal to empty string

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,6 +1,14 @@
 require "spec_helper"
 
-modeule TicTacToe
+module TicTacToe
  describe Cell do
+
+  context "#intitialize" do 
+   it "is initialized with a value of '' by default" do 
+    cell = Cell.new
+    expect(cell.value).to eq '' 
+   end 
+  end 
+
  end 
 end 
